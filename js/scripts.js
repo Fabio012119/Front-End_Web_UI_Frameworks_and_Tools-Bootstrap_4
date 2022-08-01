29cb77b02a68bdfa6d7fc15bf56a9cc530f7fc96
Fabio Saraseli <fabiooosara@gmail.com>
2022-08-01 07:30:01 -0700

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#mycarousel').carousel({interval:2000});
+  $('#carouselButton').click(function(){
+      if ($('#carouselButton').children('span').hasClass('fa-pause')){
+      $('#mycarousel').carousel('pause');
+      $('#carouselButton').children('span').removeClass('fa-pause');
+      $('#carouselButton').children('span').addClass('fa-play');
+      }
+      else if ($('#carouselButton').children('span').hasClass('fa-play')){
+          $('#mycarousel').carousel('cycle');
+          $('#carouselButton').children('span').removeClass('fa-play');
+          $('#carouselButton').children('span').addClass('fa-pause');
+          }
+
+  });
+});
+
+$('#loginLink').click(function(){
+      $('#login-modal').modal('toggle');
+  
+  });
+
+$('#reservationLink').click(function(){
+          $('#reservation-modal').modal('toggle')
+      
+      });
