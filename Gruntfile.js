7c27a790ab23f8a972028790b6d317c2bb125002
Fabio Saraseli <fabiooosara@gmail.com>
2022-08-01 10:32:46 -0700

    Grunt Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt){
 
   require('time-grunt')(grunt);
-  require('jit-grunt')(grunt);
+  require('jit-grunt')(grunt,{useminPrepare:'grunt-usemin'});
 
 
     grunt.initConfig({
@@ -34,10 +34,130 @@ module.exports = function(grunt){
             }
           }
         }
+      },
+      copy:{
+        html:{
+          files:[{
+            expand:true,
+            dot:true,
+            cwd:'./',
+            src:['*.html'],
+            dest:'dist'
+          }]
+        },
+        fonts:{
+          files:[{
+          expand:true,
+          dot:true,
+          cwd:'node_modules/font-awesome',
+          src:['fonts/*.*'],
+          dest:'dist'
+          }]
+        }
+      },
+      clean:{
+        build:{
+          src:['dist/']
+        }
+      },
+      imagemin:{
+        dynamic:{
+          files:[{
+            expand:true,
+            dot:true,
+            cwd:'./',
+            src:['img/*.{png,jpg,gif}'],
+            dest:'dist/'
+          }]
+        }
+      },
+      useminPrepare:{
+        foo:{
+          dest:'dist',
+          src:['contactus.html','aboutus.html','index.html']
+        },
+        options:{
+          flow:{
+            steps:{
+              css:['cssmin'],
+              js:['uglify']
+            },
+            post:{
+              css: [{
+                name:'cssmin',
+                createConfig: function(context,block){
+                  var generated = context.options.generated;
+                  generated.options={
+                    keepSpecialComments:0,rebase:false
+                  };
+                }
+              }]
+
+            }
+            
+          }
+        }
+      },
+      concat:{
+        options:{
+          separator:';'
+        },
+        dist:{}
+      },
+      uglify:{
+        dist:{}
+      },
+      cssmin:{
+        dist:{}
+      },
+      filerev:{
+        options:{
+          encoding:'utf8',
+          algorithm:'md5',
+          length:20
+        },
+        release:{
+          files:[{
+            src:[
+              'dist/js/*.js',
+              'dist/css/*.css'
+            ]
+          }]
+        }
+      },
+      usemin:{
+        html:['dist/contactus', 'dist/aboutus.html', 'dist/index.html'],
+        options:{
+          assetsDirs:['dist','dist/css',['dist/js']]
+        }
+      },
+      htmlmin:{
+        dist:{
+          options:{
+            collapseWhitespace:true
+          },
+          files:{
+            'dist/index.html':'dist/index.html',
+            'dist/contactus.html':'dist/contactus.html',
+            'dist/aboutus.html':'dist/aboutus.html'
+          }
+        }
       }
 
 
     });
     grunt.registerTask('css',['sass']);
     grunt.registerTask('default', ['browserSync','watch']);
+    grunt.registerTask('build', [
+      'clean',
+      'copy',
+      'imagemin',
+      'useminPrepare',
+      'concat',
+      'cssmin',
+      'uglify',
+      'filerev',
+      'usemin',
+      'htmlmin'
+    ]);
 };
